010d6724efb68070f213f209c4df17b0b98f3895
Gaurav Menghani <gauravm@fb.com>
2015-07-06 18:13:02 -0700

    Added a higher precision check, lower recall check

--- process_list.py
+++ process_list.py
@@ -1,11 +1,40 @@
 import sys
 import operator
 
-def profane_content(line, explicit_words):
+# High recall, low precision.
+def check_profane_content_by_substrings(line, explicit_words):
     for explicit_word in explicit_words:
         if explicit_word in line:
+            # print '%s found in %s' % (explicit_word, line)
             return True
     return False
+
+# Low recall, high precision
+def check_profane_content_by_word_splits(line, explicit_words):
+    word_set = set(line.split(' '))
+    for explicit_word in explicit_words:
+        if explicit_word in word_set:
+            # print '%s found in %s' % (explicit_word, line)
+            return True
+        explicit_phrase_set = explicit_word.split(' ')
+        has_all = True
+        for epm in explicit_phrase_set:
+            if epm not in word_set:
+                has_all = False
+        if has_all:
+            # print '%s (phrase) found in %s' % (explicit_word, line)
+            return True
+    return False;
+
+def profane_content(line, explicit_words, check_substrings=True):
+    punctuation_set = ['.', '-', '_', '"', ',']
+    for p in punctuation_set:
+        line = line.replace(p, ' ')
+
+    if check_substrings:
+        return check_profane_content_by_substrings(line, explicit_words)
+    else:
+        return check_profane_content_by_word_splits(line, explicit_words)
 
 def check_word_stats(wdict, line):
     for x in map(lambda(x): x.lower(), line.split(' ')):
@@ -29,13 +58,13 @@ def main(argv):
 
     profane_splits = []
     not_profane_splits = []
-
+    use_substrings = False
     wdict = {}
 
     profane_pages_file = open(file_to_process + '.porny', 'w+')
     regular_pages_file = open(file_to_process + '.regular', 'w+')
     for split in splits:
-        if profane_content(split[0].lower(), explicit_words):
+        if profane_content(split[0].lower(), explicit_words, use_substrings):
             profane_splits.append(split)
             profane_pages_file.write("{}\n".format(split[0]))
         else:
